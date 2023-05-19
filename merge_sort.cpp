a20e3cd1dcb338e5516856e3b657a2971a14e897
Joey Huang <chaoyen510220@gmail.com>
2023-05-19 13:25:55 +0800

    feat:modify merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
-int main(void){
+int main(int argc, char *argv[]){
+    cout<<argc<<endl;
+    for(int i=1;i<argc;i++){
+        cout<<argv[i]<<endl;
+    }
     cout<<"hello world"<<endl;
 }
